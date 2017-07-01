ec55d2a2820fecf55aac1f99118b158a75ed86d7
genegr <eugenio.grosso@gmail.com>
2017-07-01 16:20:47 +0200

    Update Pure-FA_prtg.py

--- Pure-FA_prtg.py
+++ Pure-FA_prtg.py
@@ -15,6 +15,7 @@
 ## Dependencies
 #
 #  purestorage       Pure Storage Python REST Client (https://github.com/purestorage/rest-client)
+#  peapy             From the PRTG internal python libraries 
 #
 
 import sys
@@ -27,25 +28,26 @@ import urllib3
 from paepy.ChannelDefinition import CustomSensorResult
 
 if __name__ == "__main__":
-	# interpret first command line parameter as json object
-	data = json.loads(sys.argv[1])
+    # interpret first command line parameter as json object
+    data = json.loads(sys.argv[1])
 
     # create sensor result
-	params = json.loads(data['params'])
-	result = CustomSensorResult("Pure Storage performance info")
-	urllib3.disable_warnings()
-	fa = purestorage.FlashArray(params['addr'], api_token=params['api_token'])
-	fainfo = fa.get(action='monitor')
-	fa.invalidate_cookie()
+    params = json.loads(data['params'])
+    result = CustomSensorResult("Pure Storage performance info")
+    urllib3.disable_warnings()
+    # gat basic statistics from the FlashArray specified in the json object
+    fa = purestorage.FlashArray(params['addr'], api_token=params['api_token'])
+    fainfo = fa.get(action='monitor')
+    fa.invalidate_cookie()
 	
     # add primary channel
-	result.add_channel(channel_name="wr sec", unit="IOPS write", value=fainfo[0]['writes_per_sec'], primary_channel=True)
-	# add additional channels
-	result.add_channel(channel_name="rd sec", unit="IOPS read", value=fainfo[0]['reads_per_sec'])
-	result.add_channel(channel_name="wr latency", unit="usec", value=fainfo[0]['usec_per_write_op'])
-	result.add_channel(channel_name="rd latency", unit="usec", value=fainfo[0]['usec_per_read_op'])
-	result.add_channel(channel_name="in sec", unit="BytesBandwidth", value=fainfo[0]['input_per_sec'])
-	result.add_channel(channel_name="out sec", unit="BytesBandwidth", value=fainfo[0]['output_per_sec'])
-	result.add_channel(channel_name="q depth", unit="avg queued", value=fainfo[0]['queue_depth'])
-	# print sensor result to std
-	print(result.get_json_result())
+    result.add_channel(channel_name="wr sec", unit="IOPS write", value=fainfo[0]['writes_per_sec'], primary_channel=True)
+    # add additional channels
+    result.add_channel(channel_name="rd sec", unit="IOPS read", value=fainfo[0]['reads_per_sec'])
+    result.add_channel(channel_name="wr latency", unit="usec", value=fainfo[0]['usec_per_write_op'])
+    result.add_channel(channel_name="rd latency", unit="usec", value=fainfo[0]['usec_per_read_op'])
+    result.add_channel(channel_name="in sec", unit="BytesBandwidth", value=fainfo[0]['input_per_sec'])
+    result.add_channel(channel_name="out sec", unit="BytesBandwidth", value=fainfo[0]['output_per_sec'])
+    result.add_channel(channel_name="q depth", unit="avg queued", value=fainfo[0]['queue_depth'])
+    # print sensor result to stdout
+    print(result.get_json_result())
