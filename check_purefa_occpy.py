d8d7fc5f362ea61b5b41d142b94e7f8bbcf34553
genegr <eugenio.grosso@gmail.com>
2018-02-01 19:45:12 +0100

    Update check_purefa_occpy.py

--- check_purefa_occpy.py
+++ check_purefa_occpy.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright (c) 2018 Pure Storage, Inc.
 #
 ## Overview
@@ -29,8 +29,13 @@ __status__ = "Production"
 
 """Pure Storage FlashArray occupancy status
 
-   Nagios plugin to retrieve the overall occupancy from a Pure Storage FlashArray.
+   Nagios plugin to retrieve the overall occupancy from a Pure Storage FlashArray or from a single volume.
    Storage occupancy indicators are collected from the target FA using the REST call.
+   The plugin has two mandatory arguments:  'endpoint', which specifies the target FA and 'apitoken', which
+   specifies the autentication token for the REST call session. A third optional parameter, 'volname' can
+   be used to check a specific named value. The optional values for the warning and critical thresholds have
+   different meausure units: they must be expressed as percentages in the case of checkig the whole flasharray
+   occupancy, while they must be integer byte units if checking a single volume.
 
 """
 
@@ -44,21 +49,34 @@ import urllib3
 _log = logging.getLogger('nagiosplugin')
 
 class PureFAoccpy(nagiosplugin.Resource):
-    """Pure Storage FlashArray overall occupancy
+    """Pure Storage FlashArray  occupancy
 
-    Calculates the overall FA storage occupancy
+    Calculates the overall FA storage occupancy or a single volume capacity.
 
     """
 
-    def __init__(self, endpoint, apitoken):
+    def __init__(self, endpoint, apitoken, volname):
         self.endpoint = endpoint
         self.apitoken = apitoken
+        self.volname = volname
+
+    @property
+    def name(self):
+        if (self.volname is None):
+            return 'PURE_FA_OCCUPANCY'
+        else:
+            return 'PURE_VOL_OCCUPANCY'
+
 
     def get_perf(self):
-        """Gets capacity counters from flasharray."""
+        """Gets performance counters from flasharray."""
         urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
         fa = purestorage.FlashArray(self.endpoint, api_token=self.apitoken)
-        fainfo = fa.get(space=True)[0]
+        if (self.volname is None):
+            fainfo = fa.get(space=True)[0]
+        else:
+            fainfo = fa.get_volume(self.volname, space=True)
+
         fa.invalidate_cookie()
         return(fainfo)
 
@@ -66,8 +84,12 @@ class PureFAoccpy(nagiosplugin.Resource):
 
         fainfo = self.get_perf()
         _log.debug('FA REST call returned "%s" ', fainfo)
-        occupancy = round(float(fainfo.get('total'))/float(fainfo.get('capacity')), 2) * 100
-        metric = nagiosplugin.Metric('occupancy', occupancy, '%', min=0, max=100)
+        if (self.volname is None):
+            occupancy = round(float(fainfo.get('total'))/float(fainfo.get('capacity')), 2) * 100
+            metric = nagiosplugin.Metric('FA occupancy', occupancy, '%', min=0, max=100, context='occupancy')
+        else:
+            occupancy = int(fainfo.get('volumes'))
+            metric = nagiosplugin.Metric(self.volname + ' occupancy', occupancy, 'B', min=0, context='occupancy')
         return metric
 
 
@@ -75,10 +97,11 @@ def parse_args():
     argp = argparse.ArgumentParser()
     argp.add_argument('endpoint', help="FA hostname or ip address")
     argp.add_argument('apitoken', help="FA api_token")
+    argp.add_argument('--vol', help="FA volume name. If omitted the whole FA occupancy is cecked")
     argp.add_argument('-w', '--warning', metavar='RANGE', default='',
-                      help='return warning if occupancy is outside RANGE')
+                      help='return warning if occupancy is outside RANGE. Value has to be expressed in percentage for the FA, while in bytes for the single volume')
     argp.add_argument('-c', '--critical', metavar='RANGE', default='',
-                      help='return critical if occupancy is outside RANGE')
+                      help='return critical if occupancy is outside RANGE. Value has to be expressed in percentage for the FA, while in bytes for the single volume')
     argp.add_argument('-v', '--verbose', action='count', default=0,
                       help='increase output verbosity (use up to 3 times)')
     argp.add_argument('-t', '--timeout', default=30,
@@ -89,7 +112,7 @@ def parse_args():
 @nagiosplugin.guarded
 def main():
     args = parse_args()
-    check = nagiosplugin.Check( PureFAoccpy(args.endpoint, args.apitoken) )
+    check = nagiosplugin.Check( PureFAoccpy(args.endpoint, args.apitoken, args.vol) )
     check.add(nagiosplugin.ScalarContext('occupancy', args.warning, args.critical))
     check.main(args.verbose, args.timeout)
 
